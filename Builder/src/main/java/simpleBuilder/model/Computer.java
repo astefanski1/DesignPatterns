fc089e9865487fa55b657160a1f7f2c8a00c44c0
astefanski1 <aleksander.stefanski@outlook.com>
2019-04-04 00:11:57 +0200

    Fluent builder implementation

--- Builder/src/main/java/simpleBuilder/model/Computer.java
+++ Builder/src/main/java/simpleBuilder/model/Computer.java
@@ -1,12 +1,21 @@
 package simpleBuilder.model;
 
+import simpleBuilder.Utils.ComputerBrand;
+import simpleBuilder.Utils.ComputerType;
+
 public class Computer {
     private String name;
     private double price;
     private Enum brand;
     private Enum computerType;
 
-    public Computer() {
+    public Computer(){}
+
+    public Computer(CompBuilder builder) {
+        this.name = builder.name;
+        this.price = builder.price;
+        this.brand = builder.brand;
+        this.computerType = builder.computerType;
     }
 
     public Computer(String name, double price, Enum brand, Enum computerType) {
@@ -56,5 +65,35 @@ public class Computer {
         display.append("Computer type: ").append(computerType.toString()).append("\n");
         display.append("Price: ").append(price).append("\n");
         return display.toString();
+    }
+
+    public class CompBuilder {
+        private String name;
+        private double price;
+        private Enum brand;
+        private Enum computerType;
+
+        public CompBuilder(ComputerType computerType) {
+            this.computerType = computerType;
+        }
+
+        public CompBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public CompBuilder price(double price) {
+            this.price = price;
+            return this;
+        }
+
+        public CompBuilder brand(ComputerBrand computerBrand) {
+            this.brand = computerBrand;
+            return this;
+        }
+
+        public Computer build() {
+            return new Computer(this);
+        }
     }
 }
